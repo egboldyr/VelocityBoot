e0e05c313adfcf8104120a214e7a8cd7a832a51c
kpetkov <kpetkov>
2018-03-06 18:18:46 +0200

    jaxws part3

--- JaxWsSpringBoot/src/main/java/wsclient/WSConsoleClient.java
+++ JaxWsSpringBoot/src/main/java/wsclient/WSConsoleClient.java
@@ -15,19 +15,15 @@ import org.apache.cxf.staxutils.StaxUtils;
 public class WSConsoleClient {
 
     public static void main(String[] args) throws Exception {
-        String address = "http://localhost:8090/services/web_service_test";
-        String request = "<q0:WebTest xmlns:q0=\"http://impl.jaxws.ws/\"></q0:WebTest>";
 
-        StreamSource source = new StreamSource(new StringReader(request));
+        String address = "http://localhost:8090/services/web_service_test";
+
         Service service = Service.create(new URL(address + "?wsdl"),
                 new QName("http://impl.jaxws.ws/" , "WebTest"));
         Dispatch<Source> disp = service.createDispatch(
                 new QName("http://impl.jaxws.ws/" , "PortTest"),
                 Source.class, Mode.PAYLOAD);
 
-        Source result = disp.invoke(source);
-        String resultAsString = StaxUtils.toString(result);
-        System.out.println(resultAsString);
-
+        /*Осталось распарсить результат :)*/
     }
 }
