ff11a626a9349653b3e1da0ecdda37e65dca8650
kpetkov <kpetkov>
2018-03-12 16:18:31 +0200

    getAll() - JS functions

--- JaxWsWebClient/src/main/resources/templates/velocity/js/index.js
+++ JaxWsWebClient/src/main/resources/templates/velocity/js/index.js
@@ -1,3 +1,4 @@
+/*Script для создания нового контакта*/
 function createNewContact(event) {
     jQuery.ajax({
         type: 'POST',
@@ -15,4 +16,32 @@ function createNewContact(event) {
             console.log( errorThrown );
         }
     });
+}
+
+/*Получение всех записей контактов из БД*/
+function getAllContacts() {
+    jQuery.ajax({
+        type: 'GET',
+        url: '/all_contacts',
+        success: function (response) {
+            processContacts(response);
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log( errorThrown );
+        }
+    });
+}
+
+function processContacts(result) {
+    var contacts    = jQuery.parseJSON(result);
+    var tab = document.getElementById("contacts");
+
+    for (var i = 0; i < contacts.length; i++) {
+        var row = tab.insertRow();
+        row.insertCell(0).innerHTML = contacts[i].id;
+        row.insertCell(1).innerHTML = contacts[i].name;
+        row.insertCell(2).innerHTML = contacts[i].surname;
+        row.insertCell(3).innerHTML = contacts[i].phone;
+        row.insertCell(4).innerHTML = contacts[i].email;
+    }
 }
